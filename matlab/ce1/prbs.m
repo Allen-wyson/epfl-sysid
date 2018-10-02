6d611164d5a73d64368c8e91e65b6058f2250063
Arne Sachtler <arne.sachtler@gmail.com>
2018-10-02 09:12:49 +0200

    Remove bib form latexmk

--- matlab/ce1/prbs.m
+++ matlab/ce1/prbs.m
@@ -12,7 +12,7 @@ q=[0,1,2,1,2,1,1,0,4,3];
 
 if n > 10, error('Maximum allowable value for n is 10');end
 if n < 2, error('Minimum allowable value for n is 2');end
-if n==8, 
+if n==8 
    for i=n+1:2^n-1, u(i)=xor(xor(u(i-n),u(i-n+1)),xor(u(i-n+2),u(i-n+7)));end
 else
     for i=n+1:2^n-1, u(i)=xor(u(i-n),u(i-n+q(n)));end
@@ -22,4 +22,3 @@ u1=[];
 for i=1:p, u1=[u,u1];end
 
 u=2*(u1-0.5)';
-
