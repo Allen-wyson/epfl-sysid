e0f6906336628159ee21cd22682d314c8f4af953
ga59fex <julia.krottenthaler@tum.de>
2018-10-09 20:22:07 +0200

    Replace ce1_3.m

--- matlab/ce1/ce1_3.m
+++ matlab/ce1/ce1_3.m
@@ -1,37 +1,67 @@
-close all
-init
+clear all;
+clc;
+close all;
+init;
+
+%% Input signal: random signal
 
 T = 250;
-Te = .5;
+Te = 0.5;
+Tend = T-Te;
 N = T/Te;
-signal = rand(N, 1)-.5;
-tmp = signal;
-tmp(2:N) = 0;
+signal = rand(N,1)-0.5;
+
+
+%% Input Toeplitz matrix
+
+tmp = zeros(size(signal,1),1);
+tmp(1,1) = signal(1,1);
+
 input = toeplitz(signal, tmp);
 
-t = (0:Te:(N-1)*Te)';
-simin.time = t;
-simin.signals.values = signal;
 
-Tend = T-Te;
-sim('ce1');
+%% Simulation 
+
+t = (0:Te:(N-1)*Te)'; 
+
+simin.signals.values = signal;
+simin.time = t;
+
+sim('ce1_1');
+%sim('ce1');
 
 output = simout.Data;
-plot(t, signal)
-hold on
-plot(t, output)
-legend('Input', 'Output')
 
-figure
+
+%% Reducing the number of equations
+
 trunc = 150;
 input = input(:,1:trunc);
 impulse_response = pinv(input) * output;
-stairs(t(1:trunc), impulse_response(1:trunc));
-grid
 
 
-% For comparison
-figure
+%% Plots 
+
+plot(t, signal, 'r');
+hold on;
+plot(t, output, 'b');
+legend('Input','Output');
+title('Signal/Input and Output');
+xlabel('Time (seconds)');
+
+
+figure;
+stairs(t(1:trunc), impulse_response(1:trunc), 'b');
+grid;
+
+hold on; 
 sys = tf(1, [1, 0.4, 4.3, 0.85, 1]);
 sys = c2d(sys, Te, 'zoh');
-impulse(Te*sys)
+impulse(Te*sys, 'r');
+
+title('Impulse response');
+legend('Impulse response by deconvolution method', 'True impulse response');
+
+
+
+
