7fa53f5780b2d44c108d66aae2968d86a6427b6e
Arne Sachtler <arne.sachtler@gmail.com>
2018-10-26 18:01:22 +0200

    Update

--- matlab/ce1/ce1_5.m
+++ matlab/ce1/ce1_5.m
@@ -1,6 +1,6 @@
-%% 1.1 Initialisation and PRBS signal 
-
-init; close all; clear all; clc;
+%% 1.1 Initialization and PRBS signal 
+init
+close all; 
 
 n= 8;
 n_periods = 8;
@@ -19,23 +19,7 @@ y = simout.Data;
 %% 1.2 Compute Fourier transform (not using the first period)
 
 period_length = 2^n - 1;
-
-fft_in = zeros(period_length, 1);
-fft_out = zeros(period_length, 1);
-
-for i = 2:n_periods
-	fft_in = fft_in + fft(Uprbs((i-1)*period_length+1:i*period_length));
-	fft_out = fft_out + fft(y((i-1)*period_length+1:i*period_length));
-end
-
-fft_in = fft_in ./ (n_periods-1);
-fft_out = fft_out ./ (n_periods-1);
-
-G = fft_out ./ fft_in;
-
-%% 1.3 Frequency vector 
-
-F = (0:1:(period_length-1))' .* (1/Te) / period_length;
+[sys, F] = estimate_frequency_response(period_length, n_periods, Uprbs, y, Te);
 
 
 %% 1.4 Compute MATLAB frd frequency response
@@ -45,7 +29,6 @@ sys = c2d(sys, Te, 'zoh');
 resp = frd(sys, 2*pi*F); % pi*F
 bode(resp);
 figure;
-
 
 %% Plot spectrum
 
@@ -61,18 +44,15 @@ ylabel 'Power';
 title 'Fourier transformations';
 grid;
 
-figure
-our = frd(G, 2*pi*F);
-bode(our);
-grid;
-
-
 %% 1.5 Comparison 
 
 figure
-bode(our,'b',resp,'r');
+hold on 
+bode(our, {0, 2*pi}, 'b')
+bode(resp, {0, 2*pi}, 'r')
 grid;
 axes_handles = findall(gcf, 'type', 'axes');
 legend(axes_handles(2),'identified', 'ground truth', 'Location', 'NorthEast');
-legend(axes_handles(3),'identified', 'ground truth', 'Location', 'NorthEast');
+%legend(axes_handles(3),'identified', 'ground truth', 'Location', 'NorthEast');
 
+disp(sprintf("Signal length: %d", N))
---
+++ matlab/ce1/estimate_frequency_response.m
@@ -0,0 +1,24 @@
+function [sys, f] = estimate_frequency_response(period_len, n_periods, in, out, Te) 
+	% period len: the length of one period
+	% n_periods: the number of periods present in the signal
+	% in: the input signal applied to the system to be identified
+	% out: response of the signal
+	% Te: the sampling interval
+	% returns: sys: a MATLAB frequency model, f: a vector of frequencies
+
+	fft_in = zeros(period_len, 1);
+	fft_out = zeros(period_len, 1);
+
+	% Skip first period --> we want stationary behavior 
+	for i = 2:n_periods
+		fft_in = fft_in + fft(in((i-1)*period_len+1:i*period_len));
+		fft_out = fft_out + fft(out((i-1)*period_len+1:i*period_len));
+	end
+
+	fft_in = fft_in ./ (n_periods-1);
+	fft_out = fft_out ./ (n_periods-1);
+
+	g = fft_out ./ fft_in; 
+	f = (0:1:(period_len-1))' .* (1/Te) / period_len;
+	sys = frd(g, 2*pi*f);
+end
