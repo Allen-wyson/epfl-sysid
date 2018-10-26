e208deb36b760b139954278ae6f3fdf46b2bd159
Arne Sachtler <arne.sachtler@gmail.com>
2018-10-26 18:35:02 +0200

    Add svgs and merge chapter 5

--- matlab/ce1/ce1_5.m
+++ matlab/ce1/ce1_5.m
@@ -19,36 +19,19 @@ y = simout.Data;
 %% 1.2 Compute Fourier transform (not using the first period)
 
 period_length = 2^n - 1;
-[sys, F] = estimate_frequency_response(period_length, n_periods, Uprbs, y, Te);
-
+[our, F] = estimate_frequency_response(period_length, n_periods, Uprbs, y, Te);
 
 %% 1.4 Compute MATLAB frd frequency response
 
 sys = tf(1, [1, 0.4, 4.3, 0.85, 1]);
 sys = c2d(sys, Te, 'zoh');
 resp = frd(sys, 2*pi*F); % pi*F
-bode(resp);
-figure;
-
-%% Plot spectrum
-
-trunc = ceil(period_length/2);
-f_trunc = F(1:trunc)*2*pi; % pi*F
-loglog(f_trunc, abs(fft_in(1:trunc)), 'b');
-hold on
-loglog(f_trunc, abs(fft_out(1:trunc)), 'r');
-loglog(f_trunc, abs(G(1:trunc)), 'g');
-legend('fft of the input', 'fft of the output', 'fft of the transfer function');
-xlabel 'Frequency [rad/s]';
-ylabel 'Power';
-title 'Fourier transformations';
-grid;
 
 %% 1.5 Comparison 
 
 figure
 hold on 
-bode(our, {0, 2*pi}, 'b')
+bode(our, {0, 2*pi}, 'bx')
 bode(resp, {0, 2*pi}, 'r')
 grid;
 axes_handles = findall(gcf, 'type', 'axes');
