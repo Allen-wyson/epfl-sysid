04dedbe03cc0a36805b7dfa8ba6c4f45e9875023
Arne Sachtler <arne.sachtler@gmail.com>
2018-10-30 15:38:06 +0100

    Update m scripts

--- matlab/ce1/ce1_6.m
+++ matlab/ce1/ce1_6.m
@@ -15,7 +15,7 @@ simin.time = t;
 sim('ce1');
 y = simout.Data;
 
-settings = ["none", N, "[.6, .6, .6]", "-";
+settings = {"none", N, "[.6, .6, .6]", "-";
 			"hann", 20, "b", "-.";
 			"hann", 50, "b", ":";
 			"hann", 150, "b", "--";
@@ -23,25 +23,25 @@ settings = ["none", N, "[.6, .6, .6]", "-";
 			"hamming", 20, "r", "-.";
 			"hamming", 50, "r", ":";
 			"hamming", 150, "r", "--";
-			"hamming", 500, "r", "-"];
+			"hamming", 500, "r", "-"};
 
 plot_titles = ['No window'];
 for i=2:size(settings,1)
-	plot_titles = [plot_titles, sprintf("Windowed (%s, M=%d)", settings(i,1), 2*str2num(settings(i,2))+1)];
+	plot_titles = [plot_titles, sprintf("Windowed (%s, M=%d)", settings{i,1}, 2*settings{i,2}+1)];
 end
 
 G = zeros(2*ceil(N/2)+1,size(settings, 1));
 f = 2*pi*(0:1:N)' .* 1/(Te*N);
 
 for i=1:size(settings, 1)
-	M = str2num(settings(i,2));
-	G(:,i) = spectral_analysis(u, y, settings(i,1), M);
+	M = settings{i,2};
+	G(:,i) = spectral_analysis(u, y, settings{i,1}, M);
 end
 
 % Plot spectrum
 for i=1:size(settings,1)
 	p = abs(G(:,i));
-	loglog(f(1:N/2), p(1:N/2), 'Color', settings(i,3), 'DisplayName', plot_titles(i), 'LineStyle', settings(i,4));
+	loglog(f(1:N/2), p(1:N/2), 'Color', settings{i,3}, 'DisplayName', plot_titles{i}, 'LineStyle', settings{i,4});
 	hold on;
 end
 
@@ -51,12 +51,11 @@ title 'Frequency response';
 grid;
 legend('Location','southwest');
 
+sys = {};
 figure
 for i=1:size(settings,1)
-	sys = frd(G(:,i), f);
-	bode(sys);
-	hold on;
+	sys{i} = frd(G(:,i), f);
 end
-
+bode(sys{:})
 legend(plot_titles);
 grid;
