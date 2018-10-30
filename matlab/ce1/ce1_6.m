65bad81169351c7aa38247e369a2394744cdc6b3
ga59fex <julia.krottenthaler@tum.de>
2018-10-30 13:46:14 +0100

    Replace ce1_6.m

--- matlab/ce1/ce1_6.m
+++ matlab/ce1/ce1_6.m
@@ -1,3 +1,11 @@
+%% Initialisation and binary random signal
+
+clear all;
+clc;
+close all;
+figure;
+method = 'hann';
+
 N = 2000;
 u = randi(2, N, 1) - 1.5;
 init
@@ -5,11 +13,14 @@ init
 Tend = (N-1)*Te;
 t = (0:Te:Tend)'; 
 
+%% Simulation
+
 simin.signals.values = u;
 simin.time = t;
 sim('ce1');
 y = simout.Data;
 
+%% Spectral analysis method
 
 cross_corr = intcor(u, y);
 auto_corr = intcor(u, u);
@@ -19,21 +30,30 @@ phi_uu = fft(auto_corr);
 G = phi_yu ./ phi_uu;
 f = 2*pi*(0:1:N)' .* (1/Te) / N;
 
+%% Plots and Windowing
+
 % Plot spectrum
 p = abs(G);
-loglog(f(1:N/2), p(1:N/2), 'yellow', 'DisplayName', 'Plain');
+loglog(f(1:N/2), p(1:N/2), 'color', [0.9 0.9 0.9], 'DisplayName', 'Plain');
 grid
 hold on
 
 % Windowed version
 
-for i=20:20:100
+for M=20:20:200
 
 	window = zeros(N+1, 1);
-	M = i;
 	low = ceil(N/2)-M;
 	up = ceil(N/2)+M;
+    if strcmp(method,'hann')
 	window(low:up) = hann(2*M+1);
+    else
+        if strcmp(method,'hamming')
+        window(low:up) = hamming(2*M+1);
+        else
+            error('Method not valid.')
+        end
+    end
 
 	cross_corr_windowed = cross_corr .* window;
 	auto_corr_windowed = auto_corr .* window;
@@ -42,7 +62,20 @@ for i=20:20:100
 	phi_uu_windowed = fft(auto_corr_windowed);
 	G_windowed = phi_yu_windowed ./ phi_uu_windowed;
 	power = abs(G_windowed);
-	loglog(f(1:N/2), power(1:N/2), 'DisplayName', sprintf('Windowed (hann, M=%d)', 2*i+1));
+    
+    if strcmp(method,'hann')
+	loglog(f(1:N/2), power(1:N/2), 'DisplayName', sprintf('Windowed (hann, M=%d)', 2*M+1));
+    else
+        if strcmp(method,'hamming')
+        loglog(f(1:N/2), power(1:N/2), 'DisplayName', sprintf('Windowed (hamming, M=%d)', 2*M+1));
+        else
+            error('Method not valid.')
+        end
+    end
 end
 
-legend;
+xlabel 'Frequency [rad/s]';
+ylabel 'Power';
+title 'Frequency response';
+grid;
+legend('Location','southwest');
