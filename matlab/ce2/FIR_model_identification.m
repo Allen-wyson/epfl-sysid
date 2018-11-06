a544aaac9a2563830e0081bcb9a2c9e292c144df
ga59fex <julia.krottenthaler@tum.de>
2018-11-06 17:06:15 +0100

    Upload New File

---
+++ matlab/ce2/FIR_model_identification.m
@@ -0,0 +1,30 @@
+function [yh,theta,sigma] = FIR_model_identification(input, output, m) 
+    % input: input signal
+    % output: output signal
+    % m: number of parameters
+    % yh: predictor for output
+    % theta: parameter vector
+    % sigma: standard deviation of the parameter estimates
+
+    %% Paramter vector
+    
+    tpl = zeros(m,1);
+    tpl(1,1) = input(1,1);
+
+    phi = toeplitz(input, tpl); % Toeplitz matrix phi
+    theta = phi \ output; % backslash operator uses Moore-Penrose pseudo inverse
+
+    %% Predicted output and loss function
+
+    yh = phi * theta; 
+    J = (norm(output-yh))^2;
+
+    %% Estimate noise variance
+
+    N = size(input, 1);
+    var_est = 1/(N-m) * J;
+
+    covar = var_est * inv(phi' * phi);
+    sigma = sqrt(diag(covar));
+
+end
