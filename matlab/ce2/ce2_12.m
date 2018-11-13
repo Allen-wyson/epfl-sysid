c008445e7715185b8cee3119765a6270266b25a2
Arne Sachtler <arne.sachtler@gmail.com>
2018-11-13 11:01:04 +0100

    Fix transfer function

--- matlab/ce2/ce2_12.m
+++ matlab/ce2/ce2_12.m
@@ -10,9 +10,7 @@ N = size(u, 1);
 
 %% Parameter vector
 
-fsum = zeros();
 phi = zeros(N,4);
-
 phi(2, :) = [-y(1), 0, u(1), 0];
 for k=3:N
     phi(k,:)= [-y(k-1),-y(k-2),u(k-1),u(k-2)];
@@ -26,10 +24,23 @@ diff = yh-y;
 J = norm(diff)^2;
 disp(sprintf("Loss: %f", J))
 
-figure;
-plot(diff);
+N = size(u,1);
+f_sampling = 1e3;
+t = 0:(1/f_sampling):(N-1)*(1/f_sampling);
+z = tf('z', 1/f_sampling);
+s = (theta(3)*(1/z) + theta(4)*(1/z^2)) / (1 + theta(1)*(1/z) + theta(2)*(1/z^2));
+ysim = lsim(s, u, t);
 
 figure;
 stairs(y,'r');
 hold on;
 stairs(yh,'b');
+grid;
+legend('Measured Output', 'Predicted Output')
+
+figure;
+stairs(y, 'r');
+hold on
+stairs(ysim, 'b');
+grid;
+legend('Measured Output', 'Simulated Output')
