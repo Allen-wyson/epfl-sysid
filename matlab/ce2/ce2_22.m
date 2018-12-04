ca6dcfdf4386b21d2a92546a06db680c08116599
ga59fex <julia.krottenthaler@tum.de>
2018-12-04 11:29:08 +0100

    Replace ce2_22.m

--- matlab/ce2/ce2_22.m
+++ matlab/ce2/ce2_22.m
@@ -3,11 +3,14 @@ clear all;
 
 load CE2.mat
 N = size(u,1);
+tsample = 0.03;
 
 in = detrend(u,'constant');
 out = detrend(y,'constant');
 
-Z = iddata(out,in,0.03);
+Z = iddata(out,in,tsample);
+
+t = [0:tsample:tsample*(N-1)];
 
 Zi = Z(1:1022);
 Zv = Z(1023:N);
@@ -27,6 +30,7 @@ figure;
 resid(Zv,Miv4);
 title('Instrumental Variable');
 
+
 Marmax = armax(Zi,[na nb na nk]);
 figure;
 resid(Zv,Marmax);
@@ -42,9 +46,28 @@ figure;
 resid(Zv,Mbj);
 title('box jenkins');
 
-Mn4sid = n4sid(Zi,[7]);
+Mn4sid = n4sid(Zi,[na]);
 figure;
 resid(Zv,Mn4sid);
 title('state space');
+
+figure;
+compare(Zi,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
+
+figure;
+compare(Zv,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
+%% 
+
+% figure;
+% compare(Z,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
+
+%% Bode
+
+figure; bode(Marx); title('Arx');
+figure; bode(Miv4); title('IV');
+figure; bode(Mbj);  title('Box jenkins');
+figure; bode(Mn4sid); title('State space'); % Sieger
+figure; bode(Moe); title('Output error');
+figure; bode(Marmax); title('Armax');
 
 
