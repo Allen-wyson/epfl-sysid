678421463e424a7190a4f42da759c15fdd30c455
ga59fex <julia.krottenthaler@tum.de>
2019-01-10 13:16:25 +0100

    Replace ce2_22.m

--- matlab/ce2/ce2_22.m
+++ matlab/ce2/ce2_22.m
@@ -15,14 +15,14 @@ t = [0:tsample:tsample*(N-1)];
 Zi = Z(1:1022);
 Zv = Z(1023:N);
 
-na = 7;
-nb = 5;
+na = 6;
+nb = 4;
 nk = 2;
 
 Marx = arx(Zi,[na nb nk]);
 figure;
 resid(Zv,Marx);
-title('arx');
+title('ARX');
 
 
 Miv4 = iv4(Zi,[na nb nk]);
@@ -34,32 +34,30 @@ title('Instrumental Variable');
 Marmax = armax(Zi,[na nb na nk]);
 figure;
 resid(Zv,Marmax);
-title('armax');
+title('ARMAX');
 
 Moe = oe(Zi,[nb na nk]);
 figure;
 resid(Zv,Moe);
-title('Output error');
+title('Output Error');
 
 Mbj = bj(Zi,[nb na na na nk]);
 figure;
 resid(Zv,Mbj);
-title('box jenkins');
+title('Box Jenkins');
 
 Mn4sid = n4sid(Zi,[na]);
 figure;
 resid(Zv,Mn4sid);
-title('state space');
+title('State Space');
 
-figure;
-compare(Zi,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
+%% 2.2.3
+
+% figure;
+% compare(Zi,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
 
 figure;
 compare(Zv,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
-%% 
-
-% figure;
-% compare(Z,Marx,Miv4,Marmax,Moe,Mbj,Mn4sid);
 
 %% Bode
 
