0b84ad2be76f7d9074bc7145e6a983ac68430e89
Arne Sachtler <arne.sachtler@gmail.com>
2019-01-11 15:16:46 +0100

    Add state space identification results

--- matlab/ce2/ce2_13.m
+++ matlab/ce2/ce2_13.m
@@ -1,61 +1,20 @@
 close all;
 
 load laserbeamdataN.mat
-N = size(u,1);
 
-r = 10;
-
-Y = zeros(r, N);
-U = zeros(r, N);
-
-for i=1:N
-	for j=1:r
-		k = i+j-1;
-		if k > N
-			Y(j,i) = 0;
-			U(j,i) = 0;
-		else
-			Y(j,i) = y(k);
-			U(j,i) = u(k);
-		end
-	end
-end
-
-U_orth = eye(N) - U' * inv(U*U') * U;
-Q = Y * U_orth;
-singular_values = svd(Q);
-
-n = sum(cumsum(singular_values)>=0.95*(sum(singular_values))); 
-disp(sprintf("Estimated order of the system: %d", n))
-O = Q(:,1:n);
-
-C = O(1,:);
-A = pinv(O(1:(r-1),:)) * O(2:r,:);
-
-
-q = tf('z');
-F = C*inv(q*eye(n) - A);
-uf = zeros(N, n);
-for i=1:n
-	uf(:,i) = lsim(F(i), u);
-end
-B = pinv(uf) * y;
-D = 0;
+[A, B, C, D] = ss_identification(u, y);
 sys = ss(A,B,C,D,1e-3);
 [y_sys, t_sys, x_sys] = lsim(sys, u);
 
 figure
-plot(singular_values);
-title 'Singular values of Q';
-
-figure
 plot(t_sys, y_sys);
 hold on
-stairs(0:1e-3:(N-1)*1e-3, y, 'r')
+stairs(0:1e-3:(size(y,1)-1)*1e-3, y, 'r')
 legend("simulated", "input data")
 grid
 
 figure
 plot(t_sys, x_sys);
+grid;
 legend("x_1", "x_2", "x_3")
 
---
+++ matlab/ce2/ss_identification.m
@@ -0,0 +1,48 @@
+function [A, B, C, D] = ss_identification(u, y)
+	%u: input signal
+	%y: output signal
+	% returns: the identified state space model matrices
+
+	N = size(u,1);
+	r = 10;
+
+	Y = zeros(r, N);
+	U = zeros(r, N);
+
+	for i=1:N
+		for j=1:r
+			k = i+j-1;
+			if k > N
+				Y(j,i) = 0;
+				U(j,i) = 0;
+			else
+				Y(j,i) = y(k);
+				U(j,i) = u(k);
+			end
+		end
+	end
+
+	% Create extended observability matrix and estimate model order
+	U_orth = eye(N) - U' * inv(U*U') * U;
+	Q = Y * U_orth;
+	singular_values = svd(Q);
+	n = sum(cumsum(singular_values)>=0.95*(sum(singular_values))); 
+	fprintf('Estimated order of the system: %d\n', n)
+	O = Q(:,1:n);
+
+	% Compute estimates for A and C
+	C = O(1,:);
+	A = pinv(O(1:(r-1),:)) * O(2:r,:);
+
+	% Construct the signals u_{f_i}
+	q = tf('z');
+	F = C*inv(q*eye(n) - A);
+	uf = zeros(N, n);
+	for i=1:n
+		uf(:,i) = lsim(F(i), u);
+	end
+
+	% Compute estimates for B and D
+	B = pinv(uf) * y;
+	D = 0;
+end
